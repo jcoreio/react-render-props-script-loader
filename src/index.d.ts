791400e2c423ff4b6d66b9d0ff1bdfc52c4cf1e4
Andy Edwards <jedwards@fastmail.com>
2024-03-26 17:40:33 -0500

    fix: fix TS type defs

--- src/index.d.ts
+++ src/index.d.ts
@@ -5,28 +5,30 @@ import * as React from 'react'
 export type State = {
   loading: boolean
   loaded: boolean
-  error: ?Error
-  promise: ?Promise<any>
+  error: Error | null | undefined
+  promise: Promise<any> | null | undefined
 }
 
 export type Props = {
   src: string
-  onLoad?: ?(() => any)
-  onError?: ?((error: Error) => any)
-  children?: ?((state: State) => ?React.Node)
-  act?: ?((action: () => void) => void)
+  onLoad?: (() => any) | null
+  onError?: ((error: Error) => any) | null
+  children?: ((state: State) => React.ReactNode | null | undefined) | null
+  act?: ((action: () => void) => void) | null
 }
 
 export function useScript(props: Props): State
 
 export declare class ScriptsRegistry {
-  scripts: Array<{ src: string }> = []
-  results: { [src: string]: { error: Error | null | undefined } } = {}
-  promises: { [src: string]: Promise<any> } = {}
+  scripts: Array<{ src: string }>
+  results: { [src: string]: { error: Error | null | undefined } }
+  promises: { [src: string]: Promise<any> }
 
-  scriptTags(options?: { nonce?: string }): React.Node
+  scriptTags(options?: { nonce?: string }): React.ReactNode
 }
 
-export const ScriptsRegistryContext: React.Context<?ScriptsRegistry>
+export const ScriptsRegistryContext: React.Context<
+  ScriptsRegistry | null | undefined
+>
 
-export default function ScriptLoader(props: Props): React.Node | null
+export default function ScriptLoader(props: Props): React.ReactElement | null
